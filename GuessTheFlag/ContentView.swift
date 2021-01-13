caee7b079867d8cc76344b1730461a98ceaf3f8d
1Soyebo <soyeboibukun@gmail.com>
2021-01-13 21:46:42 +0100

    feature: flags

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,15 +10,21 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var showingAlert = false
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        Form{
-            Button("Show Alert") {
-                        self.showingAlert = true
-                    }
-            .alert(isPresented: $showingAlert, content: {
-                Alert(title: Text("K"), message: Text("K"), dismissButton: .default(Text("K")))
-            })
+        VStack(spacing:50) {
+            Text("Tap the flag of")
+            Text(countries[correctAnswer])
+            ForEach(0 ..< 3) { number in
+                Button(action: {
+                   // flag was tapped
+                }) {
+                    Image(self.countries[number])
+                        .renderingMode(.original)
+                }
+            }
         }
 
     }
